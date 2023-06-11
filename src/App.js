c7d600a3e94b4ce6bce6b76174c5dceafaf4f443
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2023-06-11 16:26:25 -0400

    initial api integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import './App.css';
 
+//TODO: api  to offer potential misspelling corrects
+//      D3 heatmap
+//      redo in material UI
+//      integrate testing 
+
+const  getAirQuality = async (city) => {
+  try{
+    const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+    const data = await response.json()
+    console.log(data)
+  }
+  catch (error) {
+    console.error(error)
+    //set error state
+    //set air quality data to null
+  }
+}
+
 function App() {
   return (
    <h1>Hello, World!</h1>
